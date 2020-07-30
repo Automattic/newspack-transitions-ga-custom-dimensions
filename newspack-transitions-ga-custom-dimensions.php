98998146a9cf3c9d2be9cbaadf3d31a27de9bdef
Ivan Uravic <iuravic@gmail.com>
2020-07-30 10:11:25 +0200

    Update dimension ids being set by the filters

--- newspack-transitions-ga-custom-dimensions.php
+++ newspack-transitions-ga-custom-dimensions.php
@@ -14,12 +14,12 @@ if ( ! defined( 'NEWSPACK_TRANSITIONS_GA_CUSTOM_DIMENSIONS_PLUGIN_FILE' ) ) {
 	define( 'NEWSPACK_TRANSITIONS_GA_CUSTOM_DIMENSIONS_PLUGIN_FILE', __FILE__ );
 }
 
-// The GA custom dimensions' IDs.
+// The GA custom dimensions' IDs with value `sprintf( "ga:dimension%d", $dimension_id )`, e.g. 'ga:dimension1'.
 if ( ! defined( 'NEWSPACK_TRANSITIONS_GA_CUSTOM_DIMENSIONS_DIMENSION_TEAM_MEMBERSHIP_ID' ) ) {
-	define( 'NEWSPACK_TRANSITIONS_GA_CUSTOM_DIMENSIONS_DIMENSION_TEAM_MEMBERSHIP_ID', 'team_membership_id' );
+	define( 'NEWSPACK_TRANSITIONS_GA_CUSTOM_DIMENSIONS_DIMENSION_TEAM_MEMBERSHIP_ID', 'ga:dimension2' );
 }
 if ( ! defined( 'NEWSPACK_TRANSITIONS_GA_CUSTOM_DIMENSIONS_DIMENSION_USER_ID' ) ) {
-	define( 'NEWSPACK_TRANSITIONS_GA_CUSTOM_DIMENSIONS_DIMENSION_USER_ID', 'user_id' );
+	define( 'NEWSPACK_TRANSITIONS_GA_CUSTOM_DIMENSIONS_DIMENSION_USER_ID', 'ga:dimension3' );
 }
 
 require_once dirname( NEWSPACK_TRANSITIONS_GA_CUSTOM_DIMENSIONS_PLUGIN_FILE ) . '/plugin/class-plugin.php';
